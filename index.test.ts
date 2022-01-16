f0d6b30bdafc9e9071b23644a22bbaa9c82c5df5
Aarnav Tale <aarnav@tale.me>
2022-01-16 15:03:58 -0500

    fix: update tests to match new method names

--- index.test.ts
+++ index.test.ts
@@ -1,27 +1,26 @@
 import { readFile } from 'node:fs/promises'
-import { packages, release } from '.'
+import { parsePackages, parseRelease } from '.'
 
 test('release:chariz', async () => {
 	const data = await readFile('test/chariz.release', 'utf8')
-	const map = release(data)
+	const map = parseRelease(data)
 	expect(map).toBeDefined()
 })
 
 test('release:jammy', async () => {
 	const data = await readFile('test/jammy.release', 'utf8')
-	const map = release(data)
+	const map = parseRelease(data)
 	expect(map).toBeDefined()
 })
 
 test('packages:chariz', async () => {
 	const data = await readFile('test/chariz.packages', 'utf8')
-	const map = packages(data)
+	const map = parsePackages(data)
 	expect(map.length).toBe(415)
 })
 
 test('packages:jammy', async () => {
 	const data = await readFile('test/jammy.packages', 'utf8')
-	const map = packages(data)
-	console.log(map.length)
+	const map = parsePackages(data)
 	expect(map.length).toBe(6132)
 })
